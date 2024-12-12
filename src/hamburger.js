d0e05efdbcccf285a121421494da3de8863787ea
Dawid <qkari177@gmail.com>
2024-12-12 16:38:49 +0100

    burger animated

--- src/hamburger.js
+++ src/hamburger.js
@@ -1,7 +1,21 @@
 const hamMenu = document.querySelector(".ham-menu--js");
+
+const hamOne = document.querySelector(".ham-one--js");
+
+const hamTwo = document.querySelector(".ham-two--js");
+
+const hamThree = document.querySelector(".ham-three--js");
+
 const offScreenMenu = document.querySelector(".off-screen-menu--js");
 
 hamMenu.addEventListener("click", () => {
-  hamMenu.classList.toggle("active");
-    offScreenMenu.classList.toggle("-translate-x-96");
+  hamMenu.classList.toggle("relative");
+  hamMenu.classList.toggle("justify-center");
+  offScreenMenu.classList.toggle("-translate-x-96");
+  hamOne.classList.toggle("absolute5");
+  hamOne.classList.toggle("-rotate-45");
+   hamThree.classList.toggle("absolute");
+  hamThree.classList.toggle("rotate-45");
+  hamTwo.classList.toggle("hidden");
+ 
 });
