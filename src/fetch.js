21b84d4f8ef649ffe2eec4e916178b3119b521d9
Dawid <qkari177@gmail.com>
2024-12-16 04:05:24 +0100

    Fixed img

--- src/fetch.js
+++ src/fetch.js
@@ -1,5 +1,8 @@
 const username = 'Qkari17';
 const repositoriesContainer = document.querySelector('.repository--js');
+import starIcon from '/src/star-01.svg';
+import browser from '/src/browser-1.svg';
+import github from '/src/github.svg';
 
 fetch(`https://api.github.com/users/${username}/repos`)
   .then(response => {
@@ -29,7 +32,7 @@ fetch(`https://api.github.com/users/${username}/repos`)
               <div class="flex gap-4">
                 <h1 class="font-bold text-2xl">${name}</h1>
                 <span class="flex px-2 py-0.5 rounded-md bg-gray-400/10 gap-0.5 items-center">
-                  <img src="./src/star-01.svg" class="h-4 w-4">
+                  <img src="${starIcon}" class="h-4 w-4">
                   <p class="text-gray-400">${stargazers_count}</p>
                 </span>
               </div>
@@ -37,9 +40,9 @@ fetch(`https://api.github.com/users/${username}/repos`)
               <div class="flex gap-2">${topicsHTML}</div>
             </div>
             <div class="flex flex-col gap-4 items-start">
-            <a href="https://github.com/Qkari17" target="_blank" rel="noopener noreferrer" class="border-1 border-gray-700 bg-gray-900 px-5 py-4 flex gap-2 text-cyan-500 rounded-lg font-bold hover:bg-white/10"><img src="https://my-page-dawid.netlify.app/src/browser-1.svg"  class="w-6 h-6">View demo</a>
+            <a href="https://github.com/Qkari17" target="_blank" rel="noopener noreferrer" class="border-1 border-gray-700 bg-gray-900 px-5 py-4 flex gap-2 text-cyan-500 rounded-lg font-bold hover:bg-white/10"><img src="${github}"  class="w-6 h-6">View demo</a>
               <a href="${html_url}" target="_blank" rel="noopener noreferrer" class="border-1 border-gray-700 bg-gray-900 px-5 py-4 flex gap-2 text-cyan-500 rounded-lg font-bold hover:bg-white/10">
-                <img src="./src/github.svg" class="w-6 h-6">Source code
+                <img src="${browser}" class="w-6 h-6">Source code
               </a>
             </div>
           </div>
